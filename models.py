a348f528334556bed73b0a458a91db8740a3019e
maithilee2515 <114177664+maithilee2515@users.noreply.github.com>
2023-08-26 17:34:29 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/')
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    product = models.ManyToManyField(Product, related_name='categories')
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    order_date = models.DateTimeField(auto_now_add=True)
